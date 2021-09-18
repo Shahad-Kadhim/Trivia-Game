c5f2d252ee0c1995cbf2e46c0b665e274242b4f5
Amnah Ali <amnah1li@gmail.com>
2021-09-18 20:06:25 +0300

    add a function to get api category

--- app/src/main/java/com/example/triviatask/model/network/TriviaApiService.kt
+++ app/src/main/java/com/example/triviatask/model/network/TriviaApiService.kt
@@ -1,5 +1,6 @@
 package com.example.triviatask.model.network
 
+import com.example.triviatask.model.data.apiCategory.ApiCategoryResponse
 import com.example.triviatask.model.data.triviaStart.TriviaStartResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -13,7 +14,8 @@ interface TriviaApiService {
         @Query("api_category") categoryKey: String,
         @Query("api_count_global") apiCountGlobal: String,
         @Query("token") token: String,
-        @Query("api_category") apiCategory: String,
     ): Response<TriviaStartResponse>
 
+    @GET("api_category.php")
+    suspend fun getApiCategory(): Response<ApiCategoryResponse>
 }
