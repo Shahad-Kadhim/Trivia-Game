51b2a0243835c0b2b656202fe0e3c305698b9135
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-09-19 10:27:37 +0300

    delete unused code

--- app/src/main/java/com/example/triviatask/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/triviatask/ui/home/HomeFragment.kt
@@ -1,14 +1,11 @@
 package com.example.triviatask.ui.home
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import com.example.triviatask.R
 import com.example.triviatask.databinding.FragmentHomeBinding
-import com.example.triviatask.model.Repository
-import com.example.triviatask.model.State
 import com.example.triviatask.ui.base.BaseFragment
 
 class HomeFragment: BaseFragment<FragmentHomeBinding>() {
@@ -24,18 +21,5 @@ class HomeFragment: BaseFragment<FragmentHomeBinding>() {
             this.lifecycleOwner=viewLifecycleOwner
             this.viewModel=this@HomeFragment.viewModel
         }
-        viewModel.observe(Repository.getCategories(),{
-            when (it) {
-                is State.Success -> Log.i("TAG", it.toData().toString())
-                is State.Loading -> Log.i("TAG","Loading")
-                else -> Log.i("TAG","error")
-            }
-
-        },{
-            Log.i("TAG", it.message.toString())
-        })
-
     }
-
-
 }
